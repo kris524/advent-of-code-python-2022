baa93b93410c23167d68b436418cad4fa1f50f33
Kris <kristian_dilov@abv.bg>
2022-12-11 13:18:57 +0000

    day2

--- day2.py
+++ day2.py
@@ -3,7 +3,7 @@ with open("day2.txt") as f:
 res = []
 for line in lines:
     res.append(line.replace("\n", ""))
-print(res)
+# print(res)
 # shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
 # outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
 
@@ -13,26 +13,42 @@ outcome ={"lost": 0, "draw": 3, "won": 6}
 
 
 
-def game(other, yours):
+def play(other, yours):
     points = 0
     if yours == "X": # Rock
         if other == "C": # Scissors
             points += your_shape_dict[yours] + outcome["won"]
-        elif other == "B":
+        elif other == "B": #Paper
             points += your_shape_dict[yours] + outcome["lost"]
-        else: # other == "A"
+        else: # other == "A" # Rock
             points += your_shape_dict[yours] + outcome["draw"]
 
-    if yours == "X":
-        if other == "C":
+    if yours == "Y": #Paper
+        if other == "C": # Scissors 
+            points += your_shape_dict[yours] + outcome["lost"]
+        elif other == "A": #Rock
             points += your_shape_dict[yours] + outcome["won"]
-        elif other == "B":
-            points += your_shape_dict[yours] + outcome["lost"]
         else: # other == "A"
             points += your_shape_dict[yours] + outcome["draw"]
 
+    if yours == "Z": #Scissors
+        if other == "A": # Rock 
+            points += your_shape_dict[yours] + outcome["lost"]
+        elif other == "B": #Paper
+            points += your_shape_dict[yours] + outcome["won"]
+        else: # other == "A"
+            points += your_shape_dict[yours] + outcome["draw"]
+    return points
 
+# Test
+# res = ["A Y", "B X", "C Z"]
 
+final_points = 0
+for game in res:
+    entries = game.split()
+    final_points += play(entries[0], entries[1])
+
+print(final_points)
 
 
 
