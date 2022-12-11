030f1a390a4fdc4f2002d18ae35687d6f724118e
Kris <kristian_dilov@abv.bg>
2022-12-11 14:05:56 +0000

    day3

--- day3.py
+++ day3.py
@@ -1,11 +1,11 @@
-
 # Basically,
 # 1. split every string in half
 # 2. find the common letter between them (lowercase or uppercase)
-# 3. get the number of points this item has, mapping from a to z as 1 to 26 
+# 3. get the number of points this item has, mapping from a to z as 1 to 26
 # and the mapping for capital letters as well
 # just sum to total+= points got from a single string
 from typing import List
+
 with open("day3.txt") as f:
     lines = f.readlines()
 
@@ -19,14 +19,19 @@ for line in lines:
 
 def split_str(string: str) -> List[str]:
     length = len(string)
-    string1 = string[:length//2]
-    string2 = string[length//2:]
+    string1 = string[: length // 2]
+    string2 = string[length // 2 :]
     return [string1, string2]
+
 
 split_str("abcdef")
 
-common_characters = ''.join(
-    set("vJrwpWtwJgWr").intersection("hcsFMMfFFhFp")
-)
+common_characters = "".join(set("vJrwpWtwJgWr").intersection("hcsFMMfFFhFp"))
 
-print(common_characters)
+
+def get_points(letter):
+    if letter.islower():
+
+        return ord(letter) - 96
+    else:
+        return ord(letter) - 38
