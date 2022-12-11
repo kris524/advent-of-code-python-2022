b5a22029231c10a8a1c86e46663ef2eb0066da1e
Kris <kristian_dilov@abv.bg>
2022-12-11 14:00:59 +0000

    day3

--- day3.py
+++ day3.py
@@ -5,7 +5,7 @@
 # 3. get the number of points this item has, mapping from a to z as 1 to 26 
 # and the mapping for capital letters as well
 # just sum to total+= points got from a single string
-
+from typing import List
 with open("day3.txt") as f:
     lines = f.readlines()
 
@@ -15,3 +15,18 @@ for line in lines:
     res.append(line.replace("\n", ""))
 
 # print(res)
+
+
+def split_str(string: str) -> List[str]:
+    length = len(string)
+    string1 = string[:length//2]
+    string2 = string[length//2:]
+    return [string1, string2]
+
+split_str("abcdef")
+
+common_characters = ''.join(
+    set("vJrwpWtwJgWr").intersection("hcsFMMfFFhFp")
+)
+
+print(common_characters)
